017e9d0731ad0e1692ba486175982cc177c3c44d
Forrest Reiland <you@example.com>
2020-08-28 08:33:39 -0700

    add pizza constructot and toppings adjust object

---
+++ js/scripts.js
@@ -0,0 +1,29 @@
+// Business Logic //
+
+function Pizza (size, toppings) {
+  this.size = size 
+  this.toppings = toppings
+}
+
+let sizePrice = {
+  0: 7,
+  1: 9,
+  2: 13,
+  3: 16,
+  4: 21
+}
+
+// toppings should be a check box form on html.
+// for each topping checked it should add that to price 
+
+let toppingsAdjust = {
+  "cheese": 0,
+  "pepperonni": 1.50,
+  "sausage": 2,
+  "onions and peppers": 1.25,
+  "jalapenos": 0.75,
+  "garlic": 0.5,
+  "pineapple": 0.75,
+  
+  
+}
